d71d139fe7b5f5926f0948d42162cd2228cf8142
qianxi0410 <894871277@qq.com>
2022-05-22 10:00:56 +0800

    refactor: split pkgs

---
+++ cmds.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
+	"github.com/briandowns/spinner"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/urfave/cli/v2"
+)
+
+func gcuCmd(ctx *cli.Context) error {
+	if ctx.Bool("version") {
+		return versionCmd(ctx)
+	}
+
+	filePath := ctx.Args().First()
+	if filePath == "" {
+		filePath = "."
+	}
+
+	if ctx.Bool("binary") {
+		if ctx.Bool("global") {
+			filePath = filepath.Join(os.Getenv("GOPATH"), "bin")
+		}
+
+		if err := checkBinaries(filePath); err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	versions, err := getVersions(*ctx, filePath)
+	if err != nil {
+		return err
+	}
+
+	if len(versions) == 0 {
+		printAllDepLatest()
+		return nil
+	}
+
+	if ctx.Bool("all") {
+		s := spinner.New(spinner.CharSets[0], 100*time.Millisecond)
+		s.Prefix = "Updating... Please wait. "
+		if err := s.Color("cyan"); err != nil {
+			return err
+		}
+
+		s.Start()
+
+		func() {
+			s.Stop()
+			printAllLibLatest()
+		}()
+
+		for _, v := range versions {
+			if err := upgrade(v.path, v.new, filePath, ctx.Bool("rewrite") && !ctx.Bool("safe"), ctx.Bool("tidy")); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	options := make([]string, 0, len(versions))
+	m1, m2, m3 := caculateMaxLenForEachItem(versions)
+
+	for _, v := range versions {
+		options = append(options, v.String(m1, m2, m3))
+	}
+
+	idxs := make([]int, 0, len(options))
+
+	// disable MultiSelect answer output.
+	prompt := &MultiSelect{
+		survey.MultiSelect{
+			Message:  "Select the dependencies you need to upgrade: ",
+			Options:  options,
+			PageSize: ctx.Int("size"),
+		},
+	}
+	err = survey.AskOne(prompt, &idxs)
+	if err == terminal.InterruptErr {
+		printBye()
+		os.Exit(0)
+	} else if err != nil {
+		return err
+	}
+
+	s := spinner.New(spinner.CharSets[0], 100*time.Millisecond)
+	s.Prefix = "Updating... Please wait. "
+	if err := s.Color("cyan"); err != nil {
+		return err
+	}
+
+	s.Start()
+
+	func() {
+		s.Stop()
+		printPartDepLatest()
+	}()
+
+	for _, idx := range idxs {
+		if err := upgrade(versions[idx].path, versions[idx].new, filePath, ctx.Bool("rewrite") && !ctx.Bool("safe"), ctx.Bool("tidy")); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func versionCmd(_ *cli.Context) error {
+	fmt.Printf("gcu: %s\n", gcuVersion)
+	return nil
+}
+
+func listCmd(ctx *cli.Context) error {
+	filePath := ctx.Args().First()
+	if filePath == "" {
+		filePath = "."
+	}
+
+	versions, err := getVersions(*ctx, filePath)
+	if err != nil {
+		return err
+	}
+
+	if len(versions) == 0 {
+		printAllDepLatest()
+		return nil
+	}
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"lib", "current version", "latest version"})
+	for _, v := range versions {
+		t.AppendRow(table.Row{v.path, v.oldversion(), v.newVersion()})
+	}
+
+	t.Render()
+
+	return nil
+}
--- utils.go
+++ utils.go
@@ -1,6 +1,15 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/briandowns/spinner"
 	"github.com/fatih/color"
 )
 
@@ -40,4 +49,58 @@ func caculateMaxLenForEachItem(versions []version) (m1, m2, m3 int) {
 	}
 
 	return
+}
+
+type MultiSelect struct {
+	survey.MultiSelect
+}
+
+func (m MultiSelect) Cleanup(config *survey.PromptConfig, val interface{}) error {
+	return m.Render("", nil)
+}
+
+func checkBinaries(fp string) error {
+	s := spinner.New(spinner.CharSets[0], 100*time.Millisecond)
+	s.Prefix = "Updating... Please wait. "
+	if err := s.Color("cyan"); err != nil {
+		return err
+	}
+
+	s.Start()
+
+	defer func() {
+		s.Stop()
+		printAllLibLatest()
+	}()
+
+	output, err := exec.Command("go", "version", "-m", fp).Output()
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(bytes.NewReader(output))
+	paths := make([]string, 0)
+	wg := sync.WaitGroup{}
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if strings.HasPrefix(line, "path") {
+			path := strings.SplitN(line, "\t", 2)[1]
+			paths = append(paths, path)
+
+		}
+	}
+
+	wg.Add(len(paths))
+
+	for _, path := range paths {
+		go func(path string) {
+			defer wg.Done()
+			_ = exec.Command("go", "install", path+"@latest").Run()
+		}(path)
+	}
+
+	wg.Wait()
+
+	return nil
 }
